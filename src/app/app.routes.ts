75b38c7a46c9c38905873710ba53531f72e5e622
Flores Rodriguez, Max Alejandro - Externo <MaxAlejandro.FloresRodriguez@fuller.com.mx>
2026-08-25 15:48:05 -0600

    adding hash routing to github page compat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,13 @@
-import type {ILocationProvider} from "angular";
 import type {StateProvider} from "@uirouter/angularjs";
 import { AppComponent } from "@/app.component"
 
 export const routing = (
     $stateProvider: StateProvider,
-    $locationProvider: ILocationProvider,
 ) => {
-    $locationProvider.html5Mode(true);
-
     $stateProvider.state("docs", {
         abstract: true,
         component: AppComponent.$name,
     })
 }
 
-routing.$inject = ["$stateProvider", "$locationProvider"]
+routing.$inject = ["$stateProvider"]
